5949605f43446156f87ebfe5c0d83921be6a8e99
ulrik einarson <torvalde@gmail.com>
2017-01-10 16:05:23 +0100

    testing

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,13 +12,34 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styled from 'styled-components';
+import bg from './fjell.jpg';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+const Title = styled.h1`
+  font-size: 20vh;
+  text-align: center;
+  color: palevioletred;
+  padding-top: 40vh;
+`;
+
+const Frontpage = styled.div`
+  background: url(${bg}) 0 0 no-repeat;
+  background-size: 'cover';
+  width: 100%;
+  height: 100vh;
+`;
+
+export default class HomePage extends React.Component { // React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <main>
+        <Frontpage>
+          <Title>
+            MJØLFJELL
+          </Title>
+        </Frontpage>
+      <section>hkhj</section>
+      </main>
     );
   }
 }
